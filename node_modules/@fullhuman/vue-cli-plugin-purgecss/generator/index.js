5b4e348ba5b62d9e40f1fe1601b4a8bd04e8be07
Piitaaraq <peter.lovstrom@gmail.com>
2025-06-04 10:14:44 +0200

    Remove node_modules from git tracking

--- node_modules/@fullhuman/vue-cli-plugin-purgecss/generator/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (api, options) => {
-  api.extendPackage({
-    devDependencies: {
-      "@fullhuman/postcss-purgecss": "^4.0.0",
-    },
-  });
-  api.render("./templates", options);
-};
